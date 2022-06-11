432d4b3e82942733f5be9ec2c75255782472d3c4
Hyung-Keun <hyungkeun.kang@gmail.com>
2022-06-11 15:53:57 +0900

    Feature: element에 그리드 기능 추가.
    Grid.tsx-> 그리드 틀 잡기.

--- src/elements/Grid.tsx
+++ src/elements/Grid.tsx
@@ -1,6 +1,35 @@
 import React from "react";
+import styled from "styled-components";
 
-const Grid = () => {
-  return <React.Fragment></React.Fragment>;
+interface IGrid {
+  is_flex?: string;
+  width?: string;
+  margin?: string;
+  padding?: string;
+  bg?: string;
+  children: React.ReactNode;
+}
+
+const GridBox = styled.div<IGrid>`
+  width: ${(props) => props.width};
+  height: 100%;
+  box-sizing: border-box;
+  ${(props) => (props.padding ? `padding: ${props.padding};` : "")}
+  ${(props) => (props.margin ? `margin: ${props.margin};` : "")}
+  ${(props) => (props.bg ? `background-color: ${props.bg};` : "")}
+  ${(props) =>
+    props.is_flex
+      ? `display: flex; align-items: center; justify-content: space-between; `
+      : ""}
+`;
+
+const Grid = ({ is_flex, width, margin, padding, bg, children }: IGrid) => {
+  const styles = { is_flex, width, margin, padding, bg };
+  return (
+    <React.Fragment>
+      <GridBox {...styles}>{children}</GridBox>
+    </React.Fragment>
+  );
 };
+
 export default Grid;
