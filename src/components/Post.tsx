86c2c3b614c1649c9748bd5d1c93ca51e3d7fdf6
Hyung-Keun <hyungkeun.kang@gmail.com>
2022-06-11 15:39:05 +0900

    Add: Component폴더에 헤더와 포스트 생성. Header.tsx & Post.tsx

---
+++ src/components/Post.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Post = () => {
+  return (
+    <React.Fragment>
+      <div>img / nickname / time / btn</div>
+      <div>contents</div>
+      <div>image</div>
+      <div>comment cnt</div>
+    </React.Fragment>
+  );
+};
+
+export default Post;
