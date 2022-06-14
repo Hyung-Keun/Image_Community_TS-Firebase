31d6ca1439fb974e12af9257c4413b3fd5966676
Hyung-Keun <hyungkeun.kang@gmail.com>
2022-06-14 15:37:07 +0900

    Feature: 헤더 분기하기.
    Header.tsx-> 로그인상태일때와 아닐때를 구분하여, 헤더에 다른 정보 표시.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,30 @@
 import React from "react";
 
+import { getCookie, deleteCookie } from "@shared/Cookie";
+
 import { Grid, Text, Button } from "@elements";
 
 const Header = () => {
+  const [is_login, setIsLogin] = React.useState(false);
+  if (is_login) {
+    return (
+      <React.Fragment>
+        <Grid is_flex padding="4px 16px">
+          <Grid>
+            <Text margin="0px" size="24px" bold>
+              헬로
+            </Text>
+          </Grid>
+
+          <Grid is_flex>
+            <Button text="내정보"></Button>
+            <Button text="알림"></Button>
+            <Button text="로그아웃"></Button>
+          </Grid>
+        </Grid>
+      </React.Fragment>
+    );
+  }
   return (
     <React.Fragment>
       <Grid is_flex padding="4px 16px">
